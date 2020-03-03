de40f153735dc8200c3eeaaef48c76c3d4b2a26c
Rosti Readioff <45424690+RostiReadioff@users.noreply.github.com>
2020-03-03 21:10:49 +0000

    Add IK loop to the batch model scaling code
    This code successfuly runs IK for the 14 scaled .osim files. At the moment the code overwrites the output file, therefore the next step is to alter this part of the code (I think it is lines 153, 157 and all related 'output' files) to record IK results for all the models without overwriting.

--- Code/Model/ThoracoscapularShoulderPaperMaterials/Scripts/BatchScaling/RR_BatchScaling_IK.m
+++ Code/Model/ThoracoscapularShoulderPaperMaterials/Scripts/BatchScaling/RR_BatchScaling_IK.m
@@ -5,18 +5,28 @@ import org.opensim.modeling.*
 % Load Plugin needed for opening model
 Model.LoadOpenSimLibrary('C:\Users\rsk02\Desktop\IK routine\ThoracoscapularShoulderPaperMaterials\ScapulothoracicJointPlugin40\WinX64\ScapulothoracicJointPlugin40_WinX64');
 
-% Get the original model
-[modelFile,modelFilePath] = uigetfile('*.osim','Pick the the model file to be used.');
+% Get the generic model
+%[modelFile,modelFilePath] = uigetfile('*.osim','Pick the the model file to be used.');
 
 % Load the original model and initialize
-model_0 = Model(fullfile(modelFilePath, modelFile));
-model_0.initSystem;
+%model_0 = Model(fullfile(modelFilePath, modelFile));
+%model_0.initSystem;
+%% 
+% Go to the subject's folder where .trc files are located
+trc_data_folder = 'C:\Users\rsk02\Documents\GitHub\TechForParalysis\Code\Model\ThoracoscapularShoulderPaperMaterials\ExperimentalData\Markers';
+
+% Get and operate on the files
+[SetupForIK,SetupPath] = uigetfile('*.xml','Pick the IK.xml configuration file.');
+ikTool = InverseKinematicsTool([SetupPath SetupForIK]);
+
+% Specify results folder
+resultsfolder = 'output_rerun_2020_02_11';
+mkdir(resultsfolder);
 
 %% 
-[filename, filepath] = uigetfile('*.xml','Pick the scale.xml setup file.');
+[filename, filepath] = uigetfile('*.xml','Pick the scale.xml configuration file.');
 
 % Select scale tool and apply it to the xml file
-%SCTool = ScaleTool(fullfile(filepath, filename));
 SCTool = ScaleTool([filepath filename]);
 
 load('ScaleFactors.mat'); % load the data file for s.f.
@@ -110,6 +120,72 @@ SCTool.print(NewNameXML);
 
 % run xml scaling file & as a result it will create .osim file
 K=SCTool.ScaleTool.run();
+
+% IK for the experimental lab tests (i.e. elbow extension, flexion, etc)
+% Tell Tool to use the loaded model
+model = Model(fullfile(ScaledModelPath, NewNameOSIM));
+ikTool.setModel(model);
+trialsForIK = dir(fullfile(trc_data_folder, '*.trc'));
+nTrials = size(trialsForIK);
+
+% Loop through IK for all the trials
+for trial= 1:nTrials
+    
+    % Get the name of the file for this trial
+    markerFile = trialsForIK(trial).name;
+    
+    % Create name of trial from .trc file name
+    name = regexprep(markerFile,'.trc','');
+    fullpath = fullfile(trc_data_folder, markerFile);
+    
+    % Get trc data to determine time range
+    markerData = MarkerData(fullpath);
+    
+    % Get initial and intial time 
+    initial_time = markerData.getStartFrameTime();
+    final_time = markerData.getLastFrameTime();
+    
+    % Setup the ikTool for this trial
+    ikTool.setName(name);
+    ikTool.setMarkerDataFileName(fullpath);
+    ikTool.setStartTime(initial_time);
+    ikTool.setEndTime(final_time);
+    ikTool.setOutputMotionFileName(fullfile([cd '\' resultsfolder], [name '_ik.mot']));
+    
+    % Save the settings in a setup file
+    outfile = ['Setup_IK_' name '.xml'];
+    ikTool.print(fullfile(SetupPath, outfile));
+    
+    % Printing out current cycle
+    fprintf(['Performing IK on cycle # ' num2str(trial) '\n']);
+    
+    % Run IK
+    tic;
+    ikTool.run();
+    time(trial) = toc;
+
+    % Move Marker Errors + Locations + Setup File into Output Folder
+    [status, message] = movefile('*_ik_model_marker_locations.sto', [cd '\' resultsfolder]);
+    if(status ~= 1)
+        disp(['Locations file FAILED at relocating because ' message]);
+    end
+    
+    [status, message] = movefile('*_ik_marker_errors.sto', [cd '\' resultsfolder]);
+    if(status ~= 1)
+        disp(['Errors file FAILED at relocating because ' message]);
+    end
+    
+    [status, message] = movefile('Setup_IK_*.xml', [cd '\' resultsfolder]);
+    if(status ~= 1)
+        disp(['Setup file FAILED at relocating because ' message]);
+    end
+    
+end
+
+TotalTime = sum(time)/60;
+disp(['Total time = ', num2str(TotalTime), ' minutes'])
+
+
 
 i=i+1;
 end
