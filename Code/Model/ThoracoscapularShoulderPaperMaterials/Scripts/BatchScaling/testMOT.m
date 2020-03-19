909aa95b6c9bed1aa0f673bd95837c39c5d78241
Rosti Readioff <45424690+RostiReadioff@users.noreply.github.com>
2020-03-19 15:13:39 +0000

    Read .mot data, then plot and save plots for all the joints

--- Code/Model/ThoracoscapularShoulderPaperMaterials/Scripts/BatchScaling/testMOT.m
+++ Code/Model/ThoracoscapularShoulderPaperMaterials/Scripts/BatchScaling/testMOT.m
@@ -18,13 +18,80 @@ axial_rot=motData(:,16);
 elbow_flexion=motData(:,17);
 pro_sup=motData(:,18);
 
-% plot IK result vs time 
-figure (1); % fig1 showing thorax rotation in x, y & z
-plot(time, ground_thorax_rot_x,'r', time, ground_thorax_rot_y,'g', time, ground_thorax_rot_z,'b');
-xlabel('Time (s)');
-ylabel('IK (degrees)');
-legend('x-rot','y-rot','z-rot');
-title('Thorax')
+%% plot IK result vs time
 
+% fig1 showing thorax rotation & translation in x, y & z
+figure(1);
+subplot(2,1,1);
+plot(time, ground_thorax_rot_x,'-k', time, ground_thorax_rot_y,':k', time, ground_thorax_rot_z,'--k','LineWidth',1);
+xlim([2.4 4.6]);
+xlabel('Time (s)','FontWeight','bold');
+ylabel('Joint angle (^o)','FontWeight','bold');
+legend('x-rot','y-rot','z-rot', 'Location','best');
+title('Thorax Rotations & Translations','fontSize',12)
+subplot(2,1,2);
+plot(time, ground_thorax_tx,'-k', time, ground_thorax_ty,':k', time, ground_thorax_tz,'--k','LineWidth',1);
+xlim([2.4 4.6]);
+xlabel('Time (s)','FontWeight','bold');
+ylabel('Joint angle (^o)','FontWeight','bold');
+legend('x-translation','y-translation','z-translation', 'Location','best');
+savefig('Thorax_Rotations_Translations.fig');
 
+% fig2 showing clavicle rotation & translation
+figure(2);
+plot(time, clav_prot,'-k',time, clav_elev,':k','LineWidth',1);
+xlim([2.4 4.6]);
+xlabel('Time (s)','FontWeight','bold');
+ylabel('Joint angle (^o)','FontWeight','bold');
+legend('Rotation','Elevation', 'Location','best');
+title('Clavicle Rotations & Elevations','fontSize',12);
+savefig('Clavicle_Rotations_Translations.fig');
 
+% fig3 showing scapula rotations & translations
+figure(3);
+plot(time, scapula_abduction,'-k',time, scapula_elevation,'--k','LineWidth',1);
+xlim([2.4 4.6]);
+xlabel('Time (s)','FontWeight','bold');
+ylabel('Joint angle (^o)','FontWeight','bold');
+L1='Abduction';
+L2='Elevation';
+title('Scapula Rotations & Translations','fontSize',12);
+hold on
+plot(time, scapula_upward_rot,'color',[140 140 140]/255,'LineWidth',1);
+L3='Upward-rot';
+plot(time, scapula_winging,':k','LineWidth',1);
+L4='Internal-rot';
+legend(L1,L2,L3,L4,'Location','best');
+hold off
+savefig('Scapula_Rotations_Translations.fig');
+
+% fig4 showing shoulder rotations and elevations.
+%Plane elevation is any movements taken at the elevation plane, and
+% the angle at the elevation plane is zero. Shoulder elevation is
+% humeral or glenohumeral elevation(abduction). axial rotation is internal
+% and external rotation of the humerus - best looked at when the elbow is
+% slightly bent.
+figure(4)
+plot(time, plane_elv,'-k',time, shoulder_elv,':k', time, axial_rot,'--k','LineWidth',1);
+xlim([2.4 4.6]);
+xlabel('Time (s)','FontWeight','bold');
+ylabel('Joint angle (^o)','FontWeight','bold');
+title('Shoulder Rotations & Elevations','fontSize',12);
+legend('Plane-elevation','Shoulder-elevation','Axial-rotation', 'Location','best');
+savefig('Shoulder_Rotations_Elevations.fig');
+
+% fig5 showing elbow flexion, pronation and supination at the writs.
+figure(5)
+subplot(2,1,1);
+plot(time, elbow_flexion,'-k','LineWidth',1);
+xlim([2.4 4.6]);
+xlabel('Time (s)','FontWeight','bold');
+ylabel('Joint angle (^o)','FontWeight','bold');
+title('Elbow flexion, Ponation & Supination','fontSize',12);
+legend('Flexion', 'Location','best');
+subplot(2,1,2);
+plot(time, pro_sup,'-k','LineWidth',1);
+xlabel('Time (s)','FontWeight','bold');
+ylabel('Joint angle (^o)','FontWeight','bold');
+legend('Pronation & Supination', 'Location','best');
+savefig('Elbow_flex_pronation_supination.fig');
