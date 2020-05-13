ed86681e0472baa4277e890c8292ecdb3c9b6c4b
Rosti Readioff <45424690+RostiReadioff@users.noreply.github.com>
2020-05-13 17:53:37 +0100

    Modify batch scaling code to assess seven scaling methods only

--- Code/Model/ThoracoscapularShoulderPaperMaterials/Scripts/BatchScaling/RR_BatchScaling_IK.m
+++ Code/Model/ThoracoscapularShoulderPaperMaterials/Scripts/BatchScaling/RR_BatchScaling_IK.m
@@ -24,10 +24,10 @@ ikTool = InverseKinematicsTool([SetupPath SetupForIK]);
 % Select scale tool and apply it to the xml file
 SCTool = ScaleTool([filepath filename]);
 
-load('ScaleFactors_S3.mat'); % load the data file for s.f. in one subject
+load('New_ScaleFactors_S3.mat'); % load the data file for s.f. in one subject
 i=1;
 
-for ntest=1:14 % loop to go through all 14 trials for xml
+for ntest=1:7 % loop to go through all 14 trials for xml
 ij_c7(i)=ScaleFactor(1,ntest);                 % x thorax
 ij_px(i)=ScaleFactor(2,ntest);                 % y thorax
 ij_ac(i)=ScaleFactor(3,ntest);                 % z thorax
@@ -100,7 +100,7 @@ PropertyHelper.setValueDouble(1, prop_ha, 2) % changing the z-axis s.f.
 % set file name & path for the generic model
 GM=SCTool.getGenericModelMaker();
 GM_path=GM.updPropertyByIndex(0);
-PropertyHelper.setValueString('C:\Users\rsk02\Documents\GitHub\TechForParalysis\Code\Model\ThoracoscapularShoulderPaperMaterials\Model\ThoracoscapularShoulderModel02_generic_01.osim', GM_path, 0)
+PropertyHelper.setValueString('C:\Users\rsk02\Documents\GitHub\TechForParalysis\Code\Model\ThoracoscapularShoulderPaperMaterials\Model\ThoracoscapularShoulderModel02_generic_00.osim', GM_path, 0)
 
 % set file name & path for scaled .osim file
 ScaledModelPath='C:\Users\rsk02\Documents\GitHub\TechForParalysis\Code\Model\ThoracoscapularShoulderPaperMaterials\Model\';
@@ -170,7 +170,7 @@ for trial= 1:nTrials
     
     %Call plotting function for the current .mot file
     x=fullfile([[IKResultsPath '\' resultsfolder '\'],[name '_ik.mot']]);
-    PlottingMOT(x)
+    %PlottingMOT(x)
     
     % Move Marker Errors + Locations + Setup File + .osim file + xml scale file into Results Folder
     % move marker locations
