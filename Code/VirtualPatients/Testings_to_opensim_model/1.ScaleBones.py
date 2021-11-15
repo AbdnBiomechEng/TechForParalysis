dcbfdf0040fe82253391357bc8a72f81ee187eb8
Martin <martins@noventas.org>
2021-11-15 12:06:23 +0000

    Updated the muscle scaling, added bone scaling

---
+++ Code/VirtualPatients/Testings_to_opensim_model/1.ScaleBones.py
@@ -0,0 +1,299 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+From manual measurements to opensim scale config file
+
+Ideally in a mocap system with 3d coordinates of markers, we would project some of the markers to define the 3 axes of each body (as defined in 'material-scaling-Rosti.pdf', which follows best practices).
+    Then, in the scaling tool of opensim we would define which pairs of markers define which axes of each body ('measurement' method as described in https://simtk-confluence.stanford.edu:8443/display/OpenSim/Scale+Setup+File).
+    Opensim would calculate the distances, work out the scaling factors and scale the model.
+
+Here, because we only have a limited set of manual measurements (distance between two anatomical landmarks that aren't perfectly aligning with the axes),
+    we need to use the 'manualScale' scaling in opensim (where only the "ScaleSet" tag matters) instead of the 'measurements' (which is marker based) one.
+    We first need to approximate the sizes by computing the euclidean distances between the landmarks in the generic opensim model,
+    and then compare them to the manual measurements in order to find the scaling factors.
+
+Finally, this script will generate the scaling config file and scale the model using opensim.
+
+"""
+
+###############################################################################
+# Imports
+###############################################################################
+
+import sys, os, copy
+CURR_DIR = os.path.dirname(os.path.realpath(__file__))
+import pandas, pandas as pd
+import numpy as np
+import xml.etree.ElementTree as xml
+
+###############################################################################
+# Manual lengths from the excel file
+###############################################################################
+
+# load the excel file
+fileExcel = CURR_DIR+'/Subject 1 First Visit.xlsx'
+df = pd.read_excel(fileExcel, engine='openpyxl')
+
+excel_thorax_z = df.iloc[8, 3] # IJ to C7 : 130
+excel_thorax_y = df.iloc[7, 3] # IJ to PX : 145
+excel_thorax_x = df.iloc[9, 3] # IJ to AC : 165
+
+excel_scapula_x = df.iloc[18, 3] # TS to AA : 125
+excel_scapula_y = df.iloc[17, 3] # AI to TS : 130
+excel_scapula_z = df.iloc[16, 3] # AA to AC : 150
+
+excel_clavicle_z = df.iloc[19, 3] # SC to AC : 185
+
+excel_humerus_y = df.iloc[13, 3] # GH Centre to mid EpL-Epm (length of humerus) : 255
+excel_humerus_z = df.iloc[12, 3] # EpL to EpM (Elbow width) : 85
+
+excel_ulna_y = df.iloc[15, 3] # Mid RS-US to Mid EpL-EpM (ulnar length) : 240
+
+###############################################################################
+# Lengths form the generic opensim model
+###############################################################################
+
+# load the osim file
+#fileDasName = 'model-original-das3_withMarkersScaling.osim'
+fileDasName = 'model-das3-MOD2_withMarkersScaling.osim'
+fileDas = CURR_DIR+'/'+fileDasName
+tree_osim = osim = das = xml.ElementTree(file=fileDas)
+
+# function to read the osim xml file and extract the local coordinates of markers 
+def get_marker(targetMarker, verbose=False):
+
+    # Loop through the markers in the opensim file
+    for this_marker in tree_osim.findall("./Model/MarkerSet/objects/"):
+        name = this_marker.attrib['name']
+        
+        # If the name matches our target
+        if name == targetMarker:
+
+            # Extract the 'location' tag, convert to a numpy array, return it
+            locationInBody_str = this_marker.find("location").text
+            locationInBody     = np.fromstring(locationInBody_str, dtype=float, sep=' ')
+            if verbose: print("Name {} \nLocation {}".format(name,locationInBody))
+    
+            return locationInBody
+
+# Function to get the length of a vector in 3d space, defined by 2 points
+def length(a, b, millimeters=True):
+    this_length = np.linalg.norm(a - b)
+    if millimeters:
+        this_length = this_length * 1000 # from meters to millimeters
+    return this_length
+
+# Required markers
+ij                = get_marker('ij')
+ij_clavicle       = get_marker('ij_clavicle')
+c7                = get_marker('c7')
+px                = get_marker('px')
+ac                = get_marker('ac')
+aa                = get_marker('aa')
+ac                = get_marker('ac')
+ac_thorax         = get_marker('ac_thorax')
+ac_scapula        = get_marker('ac_scapula')
+ai                = get_marker('ai')
+ts                = get_marker('ts')
+gu                = get_marker('gu')
+EpM               = get_marker('EpM')
+EpL               = get_marker('EpL')
+centelbow_humerus = get_marker('centelbow_humerus')
+mid_RS_US_humerus = get_marker('mid_RS_US_humerus')
+
+osim_thorax_x = length(ij, ac_thorax)   # ideally would be ij_proj_c7 -> ij
+osim_thorax_y = length(ij, px)          # ideally would be ij_proj_px -> ij
+osim_thorax_z = length(ij, c7)          # ideally would be ij_proj_ac -> ij
+
+osim_scapula_x = length(ts, aa)         # ideally would be ai_proj_aa_z -> ai
+osim_scapula_y = length(ai, ts)         # ideally would be ai_proj_ts   -> ai
+osim_scapula_z = length(aa, ac_scapula) # ideally would be ai_proj_aa_x -> ai
+
+osim_clavicle_z = length(ij_clavicle, ac)
+
+osim_humerus_y = length(gu,  centelbow_humerus)
+osim_humerus_z = length(EpL, EpM)
+
+osim_ulna_y = length(centelbow_humerus, mid_RS_US_humerus)
+
+###############################################################################
+# Scaling factors from osim to excel - difference in the two measurements
+###############################################################################
+
+# the missing ones are taken from the available factors on other axes
+
+scale_thorax_x = excel_thorax_x / osim_thorax_x
+scale_thorax_y = excel_thorax_y / osim_thorax_y
+scale_thorax_z = excel_thorax_z / osim_thorax_z
+
+scale_scapula_x = excel_scapula_x / osim_scapula_x
+scale_scapula_y = excel_scapula_y / osim_scapula_y
+#scale_scapula_z = excel_scapula_z / osim_scapula_z
+scale_scapula_z = (scale_scapula_x + scale_scapula_y) / 2 # average of y and z
+
+scale_clavicle_z = excel_clavicle_z / osim_clavicle_z
+scale_clavicle_x = scale_clavicle_z # from clavicle z
+scale_clavicle_y = scale_clavicle_z # from clavicle z
+
+scale_humerus_y = excel_humerus_y / osim_humerus_y
+scale_humerus_z = excel_humerus_z / osim_humerus_z
+scale_humerus_x = (scale_humerus_y + scale_humerus_z) / 2 # average of y and z
+
+scale_ulna_y = excel_ulna_y / osim_ulna_y
+scale_ulna_x = scale_ulna_y   # from ulna y
+scale_ulna_z = scale_ulna_y   # from ulna y
+
+scale_radius_x = scale_ulna_y # from ulna y
+scale_radius_y = scale_ulna_y # from ulna y
+scale_radius_z = scale_ulna_y # from ulna y
+
+###############################################################################
+# Table summary
+###############################################################################
+
+print("\n##########################################################")
+print("    Excel   |   Osim")
+print("##########################################################")
+print("Thorax")
+print("x = excel {}     |   osim {}   =   scale factor {}".format(excel_thorax_x, osim_thorax_x, scale_thorax_x))
+print("y = excel {}     |   osim {}   =   scale factor {}".format(excel_thorax_y, osim_thorax_y, scale_thorax_y))
+print("z = excel {}     |   osim {}   =   scale factor {}".format(excel_thorax_z, osim_thorax_z, scale_thorax_z))
+print("Scapula")
+print("x = excel {}     |   osim {}   =   scale factor {}".format(excel_scapula_x, osim_scapula_x, scale_scapula_x))
+print("y = excel {}     |   osim {}   =   scale factor {}".format(excel_scapula_y, osim_scapula_y, scale_scapula_y))
+print("z = excel {}     |   osim {}   =   scale factor {}".format(excel_scapula_z, osim_scapula_z, scale_scapula_z))
+print("Clavicle")
+print("x = excel {}     |   osim {}   =   scale factor {}".format(excel_clavicle_z, osim_clavicle_z, scale_clavicle_z))
+print("Humerus")
+print("y = excel {}     |   osim {}   =   scale factor {}".format(excel_humerus_y, osim_humerus_y, scale_humerus_y))
+print("z = excel {}     |   osim {}   =   scale factor {}".format(excel_humerus_z, osim_humerus_z, scale_humerus_z))
+print("Ulna")
+print("y = excel {}     |   osim {}   =   scale factor {}".format(excel_ulna_y, osim_ulna_y, scale_ulna_y))
+print("##########################################################\n")
+
+###############################################################################
+# Generate config file
+###############################################################################
+
+# https://simtk-confluence.stanford.edu:8443/display/OpenSim/Scale+Setup+File
+
+# Load generic config file
+scale_configFile   = 'config-scale-generic-20211103.xml'
+scaleXML = CURR_DIR+"/"+scale_configFile
+tree1    = double = xml.ElementTree(file=scaleXML)
+root     = tree1.getroot()
+
+### Change the tags
+
+height = 1730 # in mm
+weight = 63.5 # in kg
+
+## Model file
+scale_modelFile = fileDasName
+tree1.find(".ScaleTool/GenericModelMaker/model_file").text = scale_modelFile
+
+## ModelScaler configuration
+
+# apply : true
+tree1.find(".ScaleTool/ModelScaler/apply").text = 'true'
+
+# scaling_order = manualScale
+tree1.find(".ScaleTool/ModelScaler/scaling_order").text = 'manualScale'
+
+# preserve_mass_distribution = true
+tree1.find(".ScaleTool/ModelScaler/preserve_mass_distribution").text = 'true'
+
+# test1 : output_model_file = 1.model_scaled_bones.osim
+tree1.find(".ScaleTool/ModelScaler/output_model_file").text = '1.model_scaled_bones.osim'
+# test2 : output_scale_file = two.xml
+tree1.find(".ScaleTool/ModelScaler/output_scale_file").text = ''
+
+# scaleset : all of our scaling factors
+
+obj = tree1.find(".ScaleTool/ModelScaler/ScaleSet/objects")
+
+for x in obj:
+
+    name = x.find("segment").text 
+    
+    if name == 'thorax':
+        x.find("scales").text = str(scale_thorax_x) + ' ' + str(scale_thorax_y) + ' ' + str(scale_thorax_z)
+        #x.find("scales").text = "1 1 4"
+
+    if name == 'scapula_r':
+        x.find("scales").text = str(scale_scapula_x) + ' ' + str(scale_scapula_y) + ' ' + str(scale_scapula_z)
+
+    if name == 'clavicle_r':
+        x.find("scales").text = str(scale_clavicle_x) + ' ' + str(scale_clavicle_y) + ' ' + str(scale_clavicle_z)
+
+    if name == 'humerus_r':
+        x.find("scales").text = str(scale_humerus_x) + ' ' + str(scale_humerus_y) + ' ' + str(scale_humerus_z)
+
+    if name == 'ulna_r':
+        x.find("scales").text = str(scale_ulna_x) + ' ' + str(scale_ulna_y) + ' ' + str(scale_ulna_z)
+
+    if name == 'radius_r':
+        x.find("scales").text = str(scale_radius_x) + ' ' + str(scale_radius_y) + ' ' + str(scale_radius_z)
+
+## Save the modified config file
+
+temp_config_name = 'tmp_config_file.xml'
+tree1.write(CURR_DIR+"/"+temp_config_name,xml_declaration=True, encoding='utf-8') # save with the header
+
+###############################################################################
+# Run opensim's scaling tool and generate a scaled model + visualization API
+###############################################################################
+
+import opensim
+
+# Load the model
+configFolder = CURR_DIR+"/"
+model = opensim.Model(configFolder+scale_modelFile)
+print("loaded model at {}".format(configFolder+scale_modelFile))
+
+# Run the scaling tool
+print("now will load the config file {}".format(configFolder+temp_config_name))
+scale_tool = opensim.ScaleTool(configFolder+temp_config_name)
+scale_tool.setDebugLevel(3)
+scale_tool.run()
+
+######
+# Visualization of scaled model
+######
+
+model_scaled = opensim.Model(CURR_DIR+"/1.model_scaled_bones.osim")
+for body in model_scaled.getBodyList():
+    print(body.getName())
+model_scaled.setUseVisualizer(True)
+state = model_scaled.initSystem()
+viz = model_scaled.updVisualizer().updSimbodyVisualizer()
+#viz.setBackgroundColor(osim.Vec3(0)) # white
+viz.setGroundHeight(-2)
+import time
+while 1:
+    time.sleep(1)
+    model_scaled.getVisualizer().show(state)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Code/VirtualPatients/Testings_to_opensim_model/2.ScaleMuscles.py
+++ Code/VirtualPatients/Testings_to_opensim_model/2.ScaleMuscles.py
@@ -2,12 +2,78 @@
 # -*- coding: utf-8 -*-
 """
 
+Info:
 Edits the muscles parameters in the DAS4.4 OSIM model.
 Input is a spreadsheet file containing the oxford testing scores.
 Output is a DAS4.4 osim file with scaled muscle parameters of activation (max_activation) and maximum force (fmax ou max_isometric_force).
 
 Dependencies:
 pandas, xmltodict, sympy, xldr and openpyxl (for excel xls and xlsx files respectively)
+
+#############
+Walkthrough:
+#############
+
+The input spreadsheet contains two tables.
+The first table has the names of group of muscles performing motions around the shoulder, elbow and wrist (e.g. "shoulder abduction"), as well as and Oxford score performed without stimulation (0/5).
+The second table has the names of individual muscles that are tested with electrical stimulation (0/5).
+
+This code associates [groups of muscles] to [lists of individual muscles],
+and works out each muscle's maximum force and maximum activation based on the oxford score(s) available.
+
+We have the following relations:
+muscle_force = theoretical_max_force_for_this_muscle * muscle_weakness_ratio
+with muscle_weakness = oxford_ratio_stimul / fes_activation
+
+max_control  = innervation factor (if it's a stimulated muscle)
+with innervation_factor = (oxford_ratio_vol * fes_activation) / oxford_ratio_stimul
+
+###########################
+Example 1: with FES testing
+For example, when processing the muscle delt_scap_1,
+if the participant has a weak shoulder Abduction Oxford score without stimulation  (oxford_score_volunt = 1, therefore oxford_ratio_volunt = 0.1)
+and it has a stronger shoulder Abduction Oxford score when stimulating this muscle (oxford_score_stimul = 3, therefore oxford_ratio_stimul = 0.5)
+
+Then the maximum muscle force of this muscle is:
+muscle_force = theoretical_max_force_for_this_muscle * muscle_weakness_ratio
+with muscle_weakness = oxford_ratio_stimul / fes_activation
+with fes_activation being a constant of how much excitation the FES can provide (simulate) to the muscle (around 0.7 or 0.8)
+// muscle_force = theoretical_max_force_for_this_muscle * (oxford_ratio_stimul / fes_activation)
+// muscle_force = 289                                   * (0.5                 / 0.8           )
+// muscle_force = 180.625
+
+And the maximum ability of the nerve of this muscle to send excitation is:
+max_control  = innervation factor (if it's a stimulated muscle)
+with innervation_factor = (oxford_ratio_vol * fes_activation) / oxford_ratio_stimul
+// max_control = (oxford_ratio_vol * fes_activation) / oxford_ratio_stimul)
+// max_control = (0.1              * 0.8           ) / 0.5                )
+// max_control = 0.16
+
+###########################
+Example 2: without FES testing
+For example. when processing the muscle supra_1
+the participant has a weak shoulder Abduction Oxford score without stimulation (oxford_score_volunt = 1, therefore oxford_ratio_volunt = 0.1)
+
+We have to either assume that the muscle weakness is equals to the nerve limitation,
+!2DO! or have some sort of reference based on the level of injury in order to distribute unevenly the muscle_weakness_factor and the innervation_factor
+
+The maximum muscle force of this muscle is:
+muscle_force = theoretical_max_force_for_this_muscle * muscle_weakness_ratio
+with muscle_weakness = oxford_ratio_volunt
+// muscle_force = theoretical_max_force_for_this_muscle * oxford_ratio_volunt
+// muscle_force = 120                                   * 0.1
+// muscle_force = 12
+
+And the maximum ability of the nerve of this muscle to send excitation is:
+max_control  = innervation factor
+with innervation_factor = oxford_ratio_volunt
+// max_control = 0.1
+
+#######
+2do
+#######
+
+[o] Choose how to handle muscles that have no FES testing : either a straight scaling factor based on the Oxford score, or some sort of decision-tree based on the innervation root
 
 """
 import pandas as pd
@@ -22,8 +88,9 @@ CURR_DIR = os.path.dirname(os.path.realpath(__file__))
 ######################
 
 input_oxford_file = 'oxford_original.xls'
-input_osim_file   = 'das4_v4_original.osim'
-output_osim_file  = 'generated.osim'
+#input_osim_file   = 'das4_v4_original.osim'
+input_osim_file = '1.model_scaled_bones.osim'
+output_osim_file  = '2.model_scaled_muscles.osim'
 fes_activation    = 0.8 # constant defining the activation level simulated under FES stimulation
 
 ######################
@@ -122,19 +189,25 @@ for muscle_group in table:
     # Read through muscles of this group of muscles (from table)
 
     for muscle in table[muscle_group]:
+        if muscle == "delt_scap": print("\n\n\n\n\n##############################################\n\n\n\n\n\n\n")
+
         print("\n2) Processing muscle {}".format(muscle))
 
         # Find this muscle in the osim file
         print("looking for it in osim file...")
         for x in osim_muscle_data:
-            
+
             name = x['@name']
 
             # When found, edit the muscle parameters of the osim file
             # There can be several muscle fibres to represent the same muscle so we do a partial match on the name
             if muscle in name:
 
-                print('Muscle ({}) found in osim file (under name of: {})'.format(muscle,name))              
+                if muscle == "delt_scap":
+                    print("\n")
+                    print(x)
+
+                print('Muscle ({}) found in osim file (under name of: {})'.format(muscle,name))    
 
                 # Get the original max force value (fmax)
                 print("max iso force is: {}".format(x['max_isometric_force']))
@@ -142,6 +215,9 @@ for muscle_group in table:
 
                 # Initialise the new fmax and min/max_control
                 fmax_new = min_control_new = max_control_new = ''
+                muscle_weakness_ratio = ''
+                innervation_factor = ''
+                has_fes_testing = False
 
                 # We need to know if this specific muscle has been tested with FES.
                 muscle_has_fes_testing = False                
@@ -149,41 +225,110 @@ for muscle_group in table:
                     if muscle in name_tmp:
                         muscle_has_fes_testing = True
 
+
                 # If it's the case, we use the Oxford Testing with FES to determine the new fmax and max_control
                 if muscle_has_fes_testing:
                     print("This muscle has a testing with FES. Using it to scale muscle parameters.")
                     oxford_score_stimul = oxford_stimul_dict[name_tmp]
                     print("Its oxford_score_stimul is {}".format(oxford_score_stimul))
 
+                    #the maximum muscle force of this muscle is:
+                    #muscle_force = theoretical_max_force_for_this_muscle * muscle_weakness_ratio
+                    #with muscle_weakness = oxford_ratio_stimul / fes_activation
+                    #with fes_activation being a constant of how much excitation the FES can provide (simulate) to the muscle (around 0.7 or 0.8)
+
                     # The estimated force ability fmax_new is based on the oxford ratio under stimulation
                     oxford_ratio_stimul = get_oxford_ratio(oxford_score_stimul)
-                    fmax_new = fmax_original * oxford_ratio_stimul
+                    muscle_weakness_ratio = oxford_ratio_stimul / fes_activation
+                    fmax_new = fmax_original * muscle_weakness_ratio
 
+                    #And the maximum ability of the nerve of this muscle to send excitation is:
+                    #max_control  = innervation factor (if it's a stimulated muscle)
+                    #with innervation_factor = (oxford_ratio_vol * fes_activation) / oxford_ratio_stimul
+
+                    # The estimated innervation ability is:
+
+                    innervation_factor = (oxford_ratio_manual * fes_activation) / oxford_ratio_stimul
+                    max_control_new = innervation_factor
+                    min_control_new = 0
+
+                    print("\nMax iso force goes from {} to {} (ratio {} from oxford score {}). Min_control is {} and max_control is {}\n".format(fmax_original,fmax_new,oxford_ratio_stimul,oxford_score_stimul, min_control_new, max_control_new))
+
+                    #fmax_new = (fmax_original * oxford_ratio_stimul) / fes_activation
                     # The estimated innervation ability is:
                     # We know oxford_score_manual and oxford_score_stimul
                     # oxford_score_manual = fmax_new * max_control_new
                     # oxford_score_stimul = fmax_new * fes_activation
                     # Solve for this equation:
+
+                    # From https://stackoverflow.com/questions/6789927/is-there-a-python-module-to-solve-linear-equations
+                    """
+                    print("We have two unknown : fmax_new and max_control_new")
+                    
+                    print("oxford_score_manual = fmax_new * max_control_new")
+                    print("{} = fmax_new * max_control_new".format(oxford_score_manual))
+                    print("oxford_score_stimul = fmax_new * fes_activation")
+                    print("{} = fmax_new * {}".format(oxford_score_stimul,fes_activation))
+
+                    print("Rearanged as :")
+                    print("0 = fmax_new * max_control_new - oxford_score_manual")
+                    print("0 = fmax_new * max_control_new - {}".format(oxford_score_manual))
+                    print("and")
+                    print("0 = fmax_new * fes_activation - oxford_score_stimul")
+                    print("0 = fmax_new * {} - {}".format(fes_activation,oxford_score_stimul))
+                    """
+                    """                    
+                    # To solve manually we would do, for fes_activation = 0.8, oxford_score_stimul = 4, oxford_score_manual = 2:
+                    # oxford_score_manual = fmax_new * max_control_new
+                    # oxford_score_stimul = fmax_new * fes_activation
+                    # soit
+                    # equation A : 2 = fmax_new * max_control_new
+                    # equation B : 4 = fmax_new * 0.8              --> fmax_new = 4 / 0.8 = 5
+                    # solving equation B allows us to can complete equation A:
+                    # equation A : 2 = 5 * max_control_new         --> max_control_new = 2 / 5 = 0.4
+                    """
+
+                    # Solve for this equation:
+                    """
                     x0, x1 = sympy.symbols(['x0', 'x1']) # x0 is fmax_new , x1 is max_control_new and the only one we consider here
                     result = sympy.solve([x0 * x1 - oxford_score_manual, x0 * fes_activation - oxford_score_stimul], [x0, x1])
                     max_control_new = round(float(result[0][1]),2) # result rounded to two decimals
                     min_control_new = 0
                     
                     print("\nMax iso force goes from {} to {} (ratio {} from oxford score {}). Min_control is {} and max_control is {}\n".format(fmax_original,fmax_new,oxford_ratio_stimul,oxford_score_stimul, min_control_new, max_control_new))
-
+                    """
                 # If not, we fall back to using the Oxford Testing without FES
                 else:
                     print("This muscle does not have a testing with FES, using non-FES manual testing to scale muscle parameters.")
-                    fmax_new = fmax_original * oxford_ratio_manual
-                    max_control_new = 1 * oxford_ratio_manual
-                    min_control_new = 0
                     
+                    #We have to either assume that the muscle weakness is equals to the nerve limitation,
+                    #!2DO! or have some sort of reference based on the level of injury  to distribute unevenly 
+
+                    #The maximum muscle force of this muscle is:
+                    #muscle_force = theoretical_max_force_for_this_muscle * muscle_weakness_ratio
+                    #with muscle_weakness = oxford_ratio_volunt
+
+                    muscle_weakness_ratio = oxford_ratio_manual
+                    fmax_new = fmax_original * muscle_weakness_ratio
+
+                    #And the maximum ability of the nerve of this muscle to send excitation is:
+                    #max_control  = innervation factor
+                    #with innervation_factor = oxford_ratio_volunt
+
+                    innervation_factor = oxford_ratio_manual
+                    max_control_new = innervation_factor
+                    min_control_new = 0
+
                     print("\nMax iso force goes from {} to {} (ratio {} from oxford score {}). Min_control is {} and max_control is {}\n".format(fmax_original,fmax_new,oxford_ratio_manual,oxford_score_manual, min_control_new, max_control_new))
 
                 # Save the calculated values to the list of dicts
+                x['max_isometric_force_original'] = fmax_original
                 x['max_isometric_force'] = fmax_new
                 x['min_control'] = min_control_new
                 x['max_control'] = max_control_new
+                x['muscle_weakness_ratio'] = muscle_weakness_ratio
+                x['innervation_factor'] = innervation_factor
+                x['muscle_has_fes_testing'] = muscle_has_fes_testing
 
 ######################
 # Saving as new file
