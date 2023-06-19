32cc85bdc851d2ce2b0d65f9805510746352cf92
Blana <s04db9@abdn.ac.uk>
2023-06-19 11:07:28 +0100

    Update simplified model

--- Code/Model/simplify_model.m
+++ Code/Model/simplify_model.m
@@ -1,112 +1,136 @@
 % Load the original model structure, that includes all the muscles
 load('model_struct.mat');
 
-% Get muscle names and Fmax
+% Get muscle names, fmax and mass
 elemnames = cell(model.nMus,1);
 maxforces = zeros(model.nMus,1);
+mass = zeros(model.nMus,1);
 for imus=1:model.nMus
     elemnames{imus} = model.muscles{imus}.osim_name;
     maxforces(imus) = model.muscles{imus}.fmax;
+    mass(imus) = model.muscles{imus}.mass;
 end
 
 % Create a table of element names and max forces
 mus_forces = table(maxforces,'RowNames',elemnames);
+
+% Create a table of element names and mass
+mus_mass = table(mass,'RowNames',elemnames);
 
 % New simplified model - start with the full one
 newmodel = model;
 newmodel.muscles = cell(1,1);
 imus = 1;
 
-% Now find max forces based on the elements we've decided to keep
+% Now find max forces and mass based on the elements we've decided to keep
 % - trap_scap_4: 1->6 
 newmodel.muscles(imus) = model.muscles(strcmp(elemnames,"trap_scap_4"));
 newmodel.muscles{imus}.fmax = sum(mus_forces(["trap_scap_1","trap_scap_2","trap_scap_3","trap_scap_4","trap_scap_5","trap_scap_6"],1).maxforces);
+newmodel.muscles{imus}.mass = sum(mus_mass(["trap_scap_1","trap_scap_2","trap_scap_3","trap_scap_4","trap_scap_5","trap_scap_6"],1).mass);
 imus = imus+1;
 
 % - trap_scap10: 7->11  
 newmodel.muscles(imus) = model.muscles(strcmp(elemnames,"trap_scap10"));
 newmodel.muscles{imus}.fmax = sum(mus_forces(["trap_scap_7","trap_scap_8","trap_scap_9","trap_scap10","trap_scap11"],1).maxforces);
+newmodel.muscles{imus}.mass = sum(mus_mass(["trap_scap_7","trap_scap_8","trap_scap_9","trap_scap10","trap_scap11"],1).mass);
 imus = imus+1;
 
 % - trap_clav_1: 1->2  
 newmodel.muscles(imus) = model.muscles(strcmp(elemnames,"trap_clav_1"));
 newmodel.muscles{imus}.fmax = sum(mus_forces(["trap_clav_1","trap_clav_2"],1).maxforces);
+newmodel.muscles{imus}.mass = sum(mus_mass(["trap_clav_1","trap_clav_2"],1).mass);
 imus = imus+1;
 
 % - lev_scap_1: 1->2 
 newmodel.muscles(imus) = model.muscles(strcmp(elemnames,"lev_scap_1"));
 newmodel.muscles{imus}.fmax = sum(mus_forces(["lev_scap_1","lev_scap_2"],1).maxforces);
+newmodel.muscles{imus}.mass = sum(mus_mass(["lev_scap_1","lev_scap_2"],1).mass);
 imus = imus+1;
 
 % - pect_min_2: 1->4  
 newmodel.muscles(imus) = model.muscles(strcmp(elemnames,"pect_min_2"));
 newmodel.muscles{imus}.fmax = sum(mus_forces(["pect_min_1","pect_min_2","pect_min_3","pect_min_4"],1).maxforces);
+newmodel.muscles{imus}.mass = sum(mus_mass(["pect_min_1","pect_min_2","pect_min_3","pect_min_4"],1).mass);
 imus = imus+1;
 
 % - rhomboid_3: 1->5  
 newmodel.muscles(imus) = model.muscles(strcmp(elemnames,"rhomboid_3"));
 newmodel.muscles{imus}.fmax = sum(mus_forces(["rhomboid_1","rhomboid_2","rhomboid_3","rhomboid_4","rhomboid_5"],1).maxforces);
+newmodel.muscles{imus}.mass = sum(mus_mass(["rhomboid_1","rhomboid_2","rhomboid_3","rhomboid_4","rhomboid_5"],1).mass);
 imus = imus+1;
 
 % - serr_ant_2:1->3  
 newmodel.muscles(imus) = model.muscles(strcmp(elemnames,"serr_ant_2"));
 newmodel.muscles{imus}.fmax = sum(mus_forces(["serr_ant_1","serr_ant_2","serr_ant_3"],1).maxforces);
+newmodel.muscles{imus}.mass = sum(mus_mass(["serr_ant_1","serr_ant_2","serr_ant_3"],1).mass);
 imus = imus+1;
 
 % - serr_ant_5:4->7  
 newmodel.muscles(imus) = model.muscles(strcmp(elemnames,"serr_ant_5"));
 newmodel.muscles{imus}.fmax = sum(mus_forces(["serr_ant_4","serr_ant_5","serr_ant_6","serr_ant_7"],1).maxforces);
+newmodel.muscles{imus}.mass = sum(mus_mass(["serr_ant_4","serr_ant_5","serr_ant_6","serr_ant_7"],1).mass);
 imus = imus+1;
 
 % - serr_ant_8: 8->12  
 newmodel.muscles(imus) = model.muscles(strcmp(elemnames,"serr_ant_8"));
 newmodel.muscles{imus}.fmax = sum(mus_forces(["serr_ant_8","serr_ant_9","serr_ant10","serr_ant11","serr_ant12"],1).maxforces);
+newmodel.muscles{imus}.mass = sum(mus_mass(["serr_ant_8","serr_ant_9","serr_ant10","serr_ant11","serr_ant12"],1).mass);
 imus = imus+1;
 
 % - delt_scap_3: 1->5  
 newmodel.muscles(imus) = model.muscles(strcmp(elemnames,"delt_scap_3"));
 newmodel.muscles{imus}.fmax = sum(mus_forces(["delt_scap_1","delt_scap_2","delt_scap_3","delt_scap_4","delt_scap_5"],1).maxforces);
+newmodel.muscles{imus}.mass = sum(mus_mass(["delt_scap_1","delt_scap_2","delt_scap_3","delt_scap_4","delt_scap_5"],1).mass);
 imus = imus+1;
 
 % - delt_scap10: 6->11  
 newmodel.muscles(imus) = model.muscles(strcmp(elemnames,"delt_scap10"));
 newmodel.muscles{imus}.fmax = sum(mus_forces(["delt_scap_6","delt_scap_7","delt_scap_8","delt_scap_9","delt_scap10","delt_scap11"],1).maxforces);
+newmodel.muscles{imus}.mass = sum(mus_mass(["delt_scap_6","delt_scap_7","delt_scap_8","delt_scap_9","delt_scap10","delt_scap11"],1).mass);
 imus = imus+1;
 
 % - delt_clav_2: 1->4  
 newmodel.muscles(imus) = model.muscles(strcmp(elemnames,"delt_clav_2"));
 newmodel.muscles{imus}.fmax = sum(mus_forces(["delt_clav_1","delt_clav_2","delt_clav_3","delt_clav_4"],1).maxforces);
+newmodel.muscles{imus}.mass = sum(mus_mass(["delt_clav_1","delt_clav_2","delt_clav_3","delt_clav_4"],1).mass);
 imus = imus+1;
 
 % - coracobr_2: 1->3  
 newmodel.muscles(imus) = model.muscles(strcmp(elemnames,"coracobr_2"));
 newmodel.muscles{imus}.fmax = sum(mus_forces(["coracobr_1","coracobr_2","coracobr_3"],1).maxforces);
+newmodel.muscles{imus}.mass = sum(mus_mass(["coracobr_1","coracobr_2","coracobr_3"],1).mass);
 imus = imus+1;
 
 % - infra_4: 1->6  
 newmodel.muscles(imus) = model.muscles(strcmp(elemnames,"infra_4"));
 newmodel.muscles{imus}.fmax = sum(mus_forces(["infra_1","infra_2","infra_3","infra_4","infra_5","infra_6"],1).maxforces);
+newmodel.muscles{imus}.mass = sum(mus_mass(["infra_1","infra_2","infra_3","infra_4","infra_5","infra_6"],1).mass);
 imus = imus+1;
 
 % - ter_min_2: 1->3  
 newmodel.muscles(imus) = model.muscles(strcmp(elemnames,"ter_min_2"));
 newmodel.muscles{imus}.fmax = sum(mus_forces(["ter_min_1","ter_min_2","ter_min_3"],1).maxforces);
+newmodel.muscles{imus}.mass = sum(mus_mass(["ter_min_1","ter_min_2","ter_min_3"],1).mass);
 imus = imus+1;
 
 % - ter_maj_3: 1->4  
 newmodel.muscles(imus) = model.muscles(strcmp(elemnames,"ter_maj_3"));
 newmodel.muscles{imus}.fmax = sum(mus_forces(["ter_maj_1","ter_maj_2","ter_maj_3","ter_maj_4"],1).maxforces);
+newmodel.muscles{imus}.mass = sum(mus_mass(["ter_maj_1","ter_maj_2","ter_maj_3","ter_maj_4"],1).mass);
 imus = imus+1;
 
 % - supra_3: 1->4  
 newmodel.muscles(imus) = model.muscles(strcmp(elemnames,"supra_3"));
 newmodel.muscles{imus}.fmax = sum(mus_forces(["supra_1","supra_2","supra_3","supra_4"],1).maxforces);
+newmodel.muscles{imus}.mass = sum(mus_mass(["supra_1","supra_2","supra_3","supra_4"],1).mass);
 imus = imus+1;
 
 % - subscap_6: 1->11  
 newmodel.muscles(imus) = model.muscles(strcmp(elemnames,"subscap_6"));
 newmodel.muscles{imus}.fmax = sum(mus_forces(["subscap_1","subscap_2","subscap_3","subscap_4","subscap_5","subscap_6",...
     "subscap_7","subscap_8","subscap_9","subscap10","subscap11"],1).maxforces);
+newmodel.muscles{imus}.mass = sum(mus_mass(["subscap_1","subscap_2","subscap_3","subscap_4","subscap_5","subscap_6",...
+    "subscap_7","subscap_8","subscap_9","subscap10","subscap11"],1).mass);
 imus = imus+1;
 
 % - bic_l: 1->1  
@@ -116,51 +140,61 @@ imus = imus+1;
 % - bic_b_1: 1->2  
 newmodel.muscles(imus) = model.muscles(strcmp(elemnames,"bic_b_1"));
 newmodel.muscles{imus}.fmax = sum(mus_forces(["bic_b_1","bic_b_2"],1).maxforces);
+newmodel.muscles{imus}.mass = sum(mus_mass(["bic_b_1","bic_b_2"],1).mass);
 imus = imus+1;
 
 % - tric_long_2: 1->4  
 newmodel.muscles(imus) = model.muscles(strcmp(elemnames,"tric_long_2"));
 newmodel.muscles{imus}.fmax = sum(mus_forces(["tric_long_1","tric_long_2","tric_long_3","tric_long_4"],1).maxforces);
+newmodel.muscles{imus}.mass = sum(mus_mass(["tric_long_1","tric_long_2","tric_long_3","tric_long_4"],1).mass);
 imus = imus+1;
 
 % - lat_dorsi_2: 1->3  
 newmodel.muscles(imus) = model.muscles(strcmp(elemnames,"lat_dorsi_2"));
 newmodel.muscles{imus}.fmax = sum(mus_forces(["lat_dorsi_1","lat_dorsi_2","lat_dorsi_3"],1).maxforces);
+newmodel.muscles{imus}.mass = sum(mus_mass(["lat_dorsi_1","lat_dorsi_2","lat_dorsi_3"],1).mass);
 imus = imus+1;
 
 % - lat_dorsi_4: 4->6  
 newmodel.muscles(imus) = model.muscles(strcmp(elemnames,"lat_dorsi_4"));
 newmodel.muscles{imus}.fmax = sum(mus_forces(["lat_dorsi_4","lat_dorsi_5","lat_dorsi_6"],1).maxforces);
+newmodel.muscles{imus}.mass = sum(mus_mass(["lat_dorsi_4","lat_dorsi_5","lat_dorsi_6"],1).mass);
 imus = imus+1;
 
 % - pect_maj_t_3: 1->3  
 newmodel.muscles(imus) = model.muscles(strcmp(elemnames,"pect_maj_t_3"));
 newmodel.muscles{imus}.fmax = sum(mus_forces(["pect_maj_t_1","pect_maj_t_2","pect_maj_t_3"],1).maxforces);
+newmodel.muscles{imus}.mass = sum(mus_mass(["pect_maj_t_1","pect_maj_t_2","pect_maj_t_3"],1).mass);
 imus = imus+1;
 
 % - pect_maj_t_5: 4->6  
 newmodel.muscles(imus) = model.muscles(strcmp(elemnames,"pect_maj_t_5"));
 newmodel.muscles{imus}.fmax = sum(mus_forces(["pect_maj_t_4","pect_maj_t_5","pect_maj_t_6"],1).maxforces);
+newmodel.muscles{imus}.mass = sum(mus_mass(["pect_maj_t_4","pect_maj_t_5","pect_maj_t_6"],1).mass);
 imus = imus+1;
 
 % - pect_maj_c_2: 1->2  
 newmodel.muscles(imus) = model.muscles(strcmp(elemnames,"pect_maj_c_2"));
 newmodel.muscles{imus}.fmax = sum(mus_forces(["pect_maj_c_1","pect_maj_c_2"],1).maxforces);
+newmodel.muscles{imus}.mass = sum(mus_mass(["pect_maj_c_1","pect_maj_c_2"],1).mass);
 imus = imus+1;
 
 % - tric_med_4: 1->5  
 newmodel.muscles(imus) = model.muscles(strcmp(elemnames,"tric_med_4"));
 newmodel.muscles{imus}.fmax = sum(mus_forces(["tric_med_1","tric_med_2","tric_med_3","tric_med_4","tric_med_5"],1).maxforces);
+newmodel.muscles{imus}.mass = sum(mus_mass(["tric_med_1","tric_med_2","tric_med_3","tric_med_4","tric_med_5"],1).mass);
 imus = imus+1;
 
 % - brachialis_4: 1->7  
 newmodel.muscles(imus) = model.muscles(strcmp(elemnames,"brachialis_4"));
 newmodel.muscles{imus}.fmax = sum(mus_forces(["brachialis_1","brachialis_2","brachialis_3","brachialis_4","brachialis_5","brachialis_6","brachialis_7"],1).maxforces);
+newmodel.muscles{imus}.mass = sum(mus_mass(["brachialis_1","brachialis_2","brachialis_3","brachialis_4","brachialis_5","brachialis_6","brachialis_7"],1).mass);
 imus = imus+1;
 
 % - brachiorad_2: 1->3  
 newmodel.muscles(imus) = model.muscles(strcmp(elemnames,"brachiorad_2"));
 newmodel.muscles{imus}.fmax = sum(mus_forces(["brachiorad_1","brachiorad_2","brachiorad_3"],1).maxforces);
+newmodel.muscles{imus}.mass = sum(mus_mass(["brachiorad_1","brachiorad_2","brachiorad_3"],1).mass);
 imus = imus+1;
 
 % - pron_teres_1: 1->1  
@@ -174,21 +208,25 @@ imus = imus+1;
 % - supinator_3: 1->5  
 newmodel.muscles(imus) = model.muscles(strcmp(elemnames,"supinator_3"));
 newmodel.muscles{imus}.fmax = sum(mus_forces(["supinator_1","supinator_2","supinator_3","supinator_4","supinator_5"],1).maxforces);
+newmodel.muscles{imus}.mass = sum(mus_mass(["supinator_1","supinator_2","supinator_3","supinator_4","supinator_5"],1).mass);
 imus = imus+1;
 
 % - pron_quad_2: 1->3  
 newmodel.muscles(imus) = model.muscles(strcmp(elemnames,"pron_quad_2"));
 newmodel.muscles{imus}.fmax = sum(mus_forces(["pron_quad_1","pron_quad_2","pron_quad_3"],1).maxforces);
+newmodel.muscles{imus}.mass = sum(mus_mass(["pron_quad_1","pron_quad_2","pron_quad_3"],1).mass);
 imus = imus+1;
 
 % - tric_lat_3: 1->5  
 newmodel.muscles(imus) = model.muscles(strcmp(elemnames,"tric_lat_3"));
 newmodel.muscles{imus}.fmax = sum(mus_forces(["tric_lat_1","tric_lat_2","tric_lat_3","tric_lat_4","tric_lat_5"],1).maxforces);
+newmodel.muscles{imus}.mass = sum(mus_mass(["tric_lat_1","tric_lat_2","tric_lat_3","tric_lat_4","tric_lat_5"],1).mass);
 imus = imus+1;
 
 % - anconeus_2: 1->5  
 newmodel.muscles(imus) = model.muscles(strcmp(elemnames,"anconeus_2"));
 newmodel.muscles{imus}.fmax = sum(mus_forces(["anconeus_1","anconeus_2","anconeus_3","anconeus_4","anconeus_5"],1).maxforces);
+newmodel.muscles{imus}.mass = sum(mus_mass(["anconeus_1","anconeus_2","anconeus_3","anconeus_4","anconeus_5"],1).mass);
 
 newmodel.nMus = imus;
 
