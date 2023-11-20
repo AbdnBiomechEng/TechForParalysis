00ed8d77e15e0c1934f0439e6389efc98968bf5f
Blana <s04db9@abdn.ac.uk>
2023-11-20 10:49:22 +0000

    Update copy_mass for new almost full model

--- Code/Model/Utils/copy_mass.m
+++ Code/Model/Utils/copy_mass.m
@@ -1,10 +1,29 @@
-full_model = load('model_struct.mat');
-simplified_model = load('simplified_arm_model_struct.mat');
+% full_model = load('model_struct.mat');
+% simplified_model = load('simplified_arm_model_struct.mat');
+% 
+% % Copy muscle mass for first 36 elements
+% for imus=1:36
+%     simplified_model.model.muscles{imus}.mass = full_model.model.muscles{imus}.mass;
+% end
+% 
+% model = simplified_model.model;
+% save simplified_arm_model_struct model
 
-% Copy muscle mass for first 36 elements
-for imus=1:36
+full_model = load('model_struct.mat');
+simplified_model = load('full_model.mat');
+
+% Copy muscle mass for first 83 elements
+for imus=1:83
     simplified_model.model.muscles{imus}.mass = full_model.model.muscles{imus}.mass;
 end
 
+% For short head of biceps, sum the mass of the two short heads of biceps
+simplified_model.model.muscles{84}.mass = full_model.model.muscles{84}.mass + full_model.model.muscles{85}.mass;
+
+% Now the rest
+for imus=86:138
+    simplified_model.model.muscles{imus-1}.mass = full_model.model.muscles{imus}.mass;
+end
+
 model = simplified_model.model;
-save simplified_arm_model_struct model
+save full_model model
